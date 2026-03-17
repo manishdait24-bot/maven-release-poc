57c5093a8d7427e29e07d835dd24d012189725ec
Manish Dait <daitmanish88@gmail.com>
2026-03-17 15:50:36 +0530

    initial commit
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

---
+++ src/main/java/org/example/DummyService.java
@@ -0,0 +1,8 @@
+package org.example;
+
+
+public class DummyService {
+  public String sayHi() {
+    return "Hello World";
+  }
+}
---
+++ src/test/java/org/example/DummyServiceTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyServiceTest {
+  @Test
+  void testSayHi() {
+    DummyService dummyService = new DummyService();
+    String actual = dummyService.sayHi();
+
+    Assertions.assertEquals("Hello World", actual);
+  }
+}
+
